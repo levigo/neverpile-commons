b459affa30436fc598c3ec828a19875754a4284b
Jörg Henne <j.henne@levigo.de>
2020-06-03 14:49:47 +0200

    feat: improve subject hints

--- neverpile-commons-authorization/src/main/java/com/neverpile/common/authorization/policy/impl/AuthorityAuthenticationMatcher.java
+++ neverpile-commons-authorization/src/main/java/com/neverpile/common/authorization/policy/impl/AuthorityAuthenticationMatcher.java
@@ -1,19 +1,24 @@
 package com.neverpile.common.authorization.policy.impl;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
-import com.neverpile.common.authorization.policy.AccessRule;
-
 @Component
 public class AuthorityAuthenticationMatcher implements AuthenticationMatcher {
 
+  public static final String ROLE = "role:";
+  
   @Override
   public boolean matchAuthentication(final Authentication authentication, final List<String> subjects) {
     return authentication.getAuthorities().stream().anyMatch(
-        a -> subjects.contains(AccessRule.ROLE + a.getAuthority()));
+        a -> subjects.contains(ROLE + a.getAuthority()));
   }
 
+  @Override
+  public List<Hint> getHints() {
+    return Arrays.asList(new Hint(ROLE, "a role/granted authority"));
+  }
 }
